2263fba9bcb71c279be4c8683f9f865f0f27cc8f
Luis Diaz <machadodiazluis@gmail.com>
2026-01-11 00:15:16 -0500

    feat: dropped song db-based sections, replaced with runtime configs

--- prisma/queries/songs/sections.ts
+++ prisma/queries/songs/sections.ts
@@ -1,31 +1,9 @@
 import { prisma } from "@/prisma/client";
 import { songPrismaSelect } from "@/prisma/select";
 import { serializeSong } from "@/prisma/serializer";
+import { timelineConfig } from "@/shared/lib/timelineConfig";
 import type { Song } from "@/types/song";
 import type { TimelineGroups } from "@/types/timeline";
-
-/**
- * Fetches songs belonging to a specific section.
- *
- * @example
- * ```typescript
- * const timelineSongs = await getSongsBySection('timelineSongs');
- * console.log(`Timeline: ${timelineSongs.length}`);
- * ```
- *
- * @throws {Error} If database connection fails or query execution fails
- *
- * @note Returns an empty array if no songs are found for the given section
- * @note Lyrics and AlbumTrack fields will be empty strings in the returned Song objects
- */
-export async function getSongsBySection(id: string): Promise<Song[]> {
-  const section = await prisma.section.findMany({
-    where: { name: id },
-    include: { song: { select: songPrismaSelect } },
-  });
-
-  return section.map((section) => serializeSong(section.song));
-}
 
 /**
  * Fetch the timeline songs grouped by year.
@@ -39,20 +17,27 @@ export async function getSongsBySection(id: string): Promise<Song[]> {
  * @note Songs within each year are sorted by release date (earliest first)
  */
 export async function getTimelineSongs(): Promise<TimelineGroups[]> {
-  const songs = await getSongsBySection("timelineSongs");
+  const songs = await prisma.song.findMany({
+    where: {
+      id: { in: timelineConfig },
+    },
+    select: songPrismaSelect,
+    orderBy: {
+      releaseDate: "asc",
+    },
+  });
 
-  const songsByYear = songs.reduce<Record<number, Song[]>>((group, song) => {
+  const grouped: Record<number, Song[]> = {};
+
+  for (const rawSong of songs) {
+    const song = serializeSong(rawSong);
     const year = new Date(song.releaseDate).getFullYear();
-    (group[year] ??= []).push(song);
-    return group;
-  }, {});
 
-  return Object.entries(songsByYear)
-    .map(([year, yearSongs]) => ({
-      year: Number(year),
-      songs: yearSongs.toSorted((a, b) =>
-        a.releaseDate.localeCompare(b.releaseDate),
-      ),
-    }))
-    .toSorted((a, b) => a.year - b.year);
+    (grouped[year] ??= []).push(song);
+  }
+
+  return Object.entries(grouped).map(([year, songs]) => ({
+    year: Number(year),
+    songs,
+  }));
 }
